02fd34f2027ba85a6a10b98e66cf8b6594e8a3a1
VQT <quoctuan02101@gmail.com>
2024-11-28 23:57:20 +0700

    update frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import { AuthProvide } from './context/AuthContext';
-import Loading from './components/Loading';
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import { AuthProvide } from './context/AuthContext'
+import { useEffect, useState } from 'react'
+import Loading from './components/Loading'
 
 function App() {
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+
     const timer = setTimeout(() => {
-      setLoading(false); // Stop loading after 2 seconds
-    }, 2000);
+      setLoading(false);
+    }, 2000); 
+
+    // Cleanup timer
     return () => clearTimeout(timer);
   }, []);
 
-  // Show loading screen
   if (loading) {
-    return <Loading />;
+    return <Loading />; 
   }
 
+
   return (
-    <AuthProvide>
-      <Navbar />
-      <main className="min-h-screen px-4 py-6 mx-auto max-w-screen-2xl font-primary">
-        <Outlet />
-      </main>
-      <Footer />
-    </AuthProvide>
-  );
+    <>
+      <AuthProvide>
+        <Navbar />
+        <main className='min-h-screen px-4 py-6 mx-auto max-w-screen-2xl font-primary'>
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvide>
+
+    </>
+  )
 }
 
-export default App;
+export default App
